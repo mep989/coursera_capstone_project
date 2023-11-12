e6a213250b7e6eb17e39c2a15156f987fdbe9e41
Michael Pokorny <mep989@gmail.com>
2023-11-12 16:01:03 -0500

    styles nav and header, uses scss, preloads fonts

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "index.css";
+import "@fontsource/karla";
+import "@fontsource/markazi-text";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
+
+import "index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
