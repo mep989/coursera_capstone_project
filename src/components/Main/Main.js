1556b64b9277cf3bcae26711cef3482a1c8ddee9
mep989 <mep989@protonmail.com>
2023-11-12 23:12:27 -0500

    Styles the main section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,53 +1,62 @@
-import Button from "react-bootstrap/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { v4 } from "uuid";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import greekSalad from "images/greek_salad.jpg";
 import bruchetta from "images/bruchetta.svg";
 import lemonDessert from "images/lemon_dessert.jpg";
+import Card from "../MainCard/MainCard";
+
+import "./Main.scss";
+
+const cardValues = [
+  {
+    title: "Greek salad",
+    imageSrc: greekSalad,
+    description: `The famous greek salad of crispy lettuce, peppers, olives and
+                  our Chicago style feta cheese, garnished with crunchy garlic
+                  and rosemary croutons.`,
+    price: "$12.99",
+  },
+  {
+    title: "Bruchetta",
+    imageSrc: bruchetta,
+    description: `Our Bruschetta is made from grilled bread that has been
+                  smeared with garlic and seasoned with salt and olive oil.`,
+    price: "$5.99",
+  },
+  {
+    title: "Lemon Dessert",
+    imageSrc: lemonDessert,
+    description: `This comes straight from grandma&apos;s recipe book, every
+                  last ingredient has been sourced and is as authentic as can be
+                  imagined.`,
+    price: "$5.00",
+  },
+];
+
+const cards = cardValues.map((props) => {
+  return (
+    <Col key={v4()}>
+      <Card {...props} />
+    </Col>
+  );
+});
 
 function Main() {
   return (
     <main>
-      <section className="top_section">
-        <h1>This weeks specials!</h1>
-        <Button>Online Menu</Button>
-      </section>
-      <section className="bottom_section">
-        <article className="bottom">
-          <img src={greekSalad} alt="Greek salad" width="400" />
-          <h3>Greek salad</h3>
-          <p>
-            The famous greek salad of crispy lettuce, peppers, olives and our
-            Chicago style feta cheese, garnished with crunchy garlic and
-            rosemary croutons.
-          </p>
-          <Button variant="link">
-            Order a delivery <FontAwesomeIcon icon={faTruck} />
-          </Button>
-        </article>
-        <article className="bottom">
-          <img src={bruchetta} alt="Bruchetta" width="400" />
-          <h3>Bruchetta</h3>
-          <p>
-            Our Bruschetta is made from grilled bread that has been smeared with
-            garlic and seasoned with salt and olive oil.
-          </p>
-          <Button variant="link">
-            Order a delivery <FontAwesomeIcon icon={faTruck} />
-          </Button>
-        </article>
-        <article className="bottom">
-          <img src={lemonDessert} alt="Lemon Dessert" width="400" />
-          <h3>Lemon Dessert</h3>
-          <p>
-            This comes straight from grandma&apos;s recipe book, every last
-            ingredient has been sourced and is as authentic as can be imagined.
-          </p>
-          <Button variant="link">
-            Order a delivery <FontAwesomeIcon icon={faTruck} />
-          </Button>
-        </article>
-      </section>
+      <Container>
+        <Row>
+          <Col xs={7}>
+            <h1>This weeks specials!</h1>
+          </Col>
+          <Col className="right-align">
+            <Button>
+              <span>Online Menu</span>
+            </Button>
+          </Col>
+        </Row>
+        <Row className="bottom-row">{cards}</Row>
+      </Container>
     </main>
   );
 }
