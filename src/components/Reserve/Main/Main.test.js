1d201c672eb34c62810071783efd6fc59c46d548
mep989 <mep989@protonmail.com>
2024-01-29 01:22:47 -0500

    Updates unit tests to factor in api calls

--- src/components/Reserve/Main/Main.test.js
+++ src/components/Reserve/Main/Main.test.js
@@ -1,20 +1,31 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { fetchAPI } from "utils/fakeApi";
 import Main from "./Main";
+
+const mockUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockUsedNavigate,
+}));
 
 test("initializeTimes returns the expected result", () => {
   render(<Main />);
-  const expectedOutput = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const date = new Date();
+  const expectedOutput = JSON.parse(fetchAPI(date)).availableTimes;
   expectedOutput.forEach((time) => {
     expect(screen.getByRole("option", { name: time })).toBeInTheDocument();
   });
 });
 
-test("updateTimes returns the expected result", () => {
+test("updateTimes returns the expected result", async () => {
   const { getByLabelText } = render(<Main />);
   fireEvent.change(getByLabelText("Choose date:"), {
-    target: { value: "01/01/1998" },
+    target: { value: "2050-01-01" },
   });
-  const expectedOutput = ["15:00", "16:00", "17:00", "18:00", "19:00", "20:00"];
+
+  const response = await fetchAPI(new Date(2050, 0, 1));
+  const expectedOutput = JSON.parse(response).availableTimes;
+
   expectedOutput.forEach((time) => {
     expect(screen.getByRole("option", { name: time })).toBeInTheDocument();
   });
